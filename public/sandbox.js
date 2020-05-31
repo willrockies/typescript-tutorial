09a8d23eba43e57cf376e71b05110727210dd379
Wilson Alves <wlsn.ads@gmail.com>
2020-05-31 15:13:57 -0300

    lesson-7

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,2 +1,18 @@
 "use strict";
-console.log('Mario');
+var greet;
+//greet = 'hello';
+greet = function () {
+    console.log('hello, again');
+};
+var add = function (a, b, c) {
+    if (c === void 0) { c = 10; }
+    console.log(a + b);
+    console.log(c); // valor 10 como default
+};
+// add(5, 10, '20');
+add(5, 10);
+var minus = function (a, b) {
+    return a - b;
+};
+var result = minus(10, 7);
+console.log(result);
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,1 +1,22 @@
-console.log('Mario');
+let greet: Function;
+//greet = 'hello';
+
+greet = () => {
+  console.log('hello, again');
+}
+
+const add = (a: number, b: number, c: number | string = 10):void => {
+  console.log(a + b);
+  console.log(c); // valor 10 como default
+}
+
+// add(5, 10, '20');
+add(5,10);
+
+const minus = (a:number, b:number): number => {
+  return a - b;
+
+}
+
+let result = minus(10, 7);
+console.log(result);
