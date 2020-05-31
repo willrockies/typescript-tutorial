0d9151a9e9443597912859168766df7511e27859
Wilson Alves <wlsn.ads@gmail.com>
2020-05-31 14:23:41 -0300

    lesson 2

---
+++ sandbox.js
@@ -0,0 +1,13 @@
+var character = 'Mario';
+var age = 30;
+var isBlackBelt = false;
+//character = 20;
+character = 'Luigi';
+//age = yoshi;
+age = 20;
+//isBlackBelt = 'yes'
+isBlackBelt = true;
+var circ = function (diameter) {
+    return diameter * Math.PI;
+};
+console.log(circ(7.5));
---
+++ sandbox.ts
@@ -0,0 +1,18 @@
+let character = 'Mario';
+let age = 30;
+let isBlackBelt = false;
+
+//character = 20;
+character = 'Luigi';
+
+//age = yoshi;
+age = 20;
+
+//isBlackBelt = 'yes'
+isBlackBelt = true
+
+const circ = (diameter:number) => {
+  return diameter * Math.PI;
+}
+
+console.log(circ(7.5))
